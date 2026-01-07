07c8dcbbe2a1299a5189eddef8f6bcc24cca192b
fidelisdaleck <fidelisdaleck386@gmail.com>
2026-01-07 15:05:40 +0100

    Modification des fichiers JS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const reveals = document.querySelectorAll('.reveal');
+// effet reveal
 
+const reveals = document.querySelectorAll('.reveal');
         function revealElements() {
         for (let i = 0; i < reveals.length; i++) {
             const windowHeight = window.innerHeight;
@@ -14,3 +15,23 @@ const reveals = document.querySelectorAll('.reveal');
 
         window.addEventListener('scroll', revealElements);
         revealElements();
+
+//Gestion du formulaire whatsApp
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const nom = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const sujet = document.getElementById("sujet").value;
+    const message = document.getElementById("message").value;
+
+    const numeroWhatsApp = "237680279842";
+    const texte = 'Bonjour, je vous contacte depuis votre site Bunkys.%0A%0A' + 
+    'Nom: ${nom}%0A' + 
+    'Email: ${email}%0A' +
+    'Sujet: ${sujet}%0A' +
+    'Message: ${message}%0A';
+
+    const url = 'https://wa.me/${numeroWhatsApp}?text=${text}';
+    window.open(url, "_blank");
+});
